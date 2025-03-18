8f7f9ad72c5770549c42c43c009ec9ae23d30379
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-03-18 23:03:18 +0000

    backend - error middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 
 import connectDB from './config/database.config.js'
 
+import { notFound, errorHandler } from './middleware/error.middleware.js'
+
 dotenv.config()
 
 const port = process.env.PORT || 5000
@@ -12,5 +14,8 @@ connectDB()
 const app = express()
 
 app.use('/', () => console.log('Server is ready!'))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server has stated on port: ${port}`))
